e53e024347362891697a1ca631da57ed0d5081a2
Baaber <tatykulov@gmail.com>
2023-04-07 17:10:17 -0500

    change 3

--- src/main/java/org/example/secondPackage.java
+++ src/main/java/org/example/secondPackage.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class secondPackage {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("change");
+        System.out.println("change 2");
+    }
 }
